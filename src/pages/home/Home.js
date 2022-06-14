0082001ba8424d8098346d595185ee144125ed9c
EvadeJong <eefjuh.de.jong@gmail.com>
2022-06-14 11:13:39 +0200

    added text and image to homepage

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import styles from './Home.module.css';
 import Button from "../../components/button/Button";
 import { useHistory } from "react-router-dom";
+import {ReactComponent as Icecream} from "../../assets/Icecream.svg";
 
 function Home(){
 
@@ -14,8 +15,17 @@ function Home(){
 
     return(
         <main className={styles.pageOuterContainer}>
+            <Icecream />
             <div className={styles.pageInnerContainer}>
-                Dit is de home pagina
+
+                <h1>Welcome</h1>
+
+                    <h2>
+                        Giggles is an app that fetches funny (and really terrible) jokes  to put a smile on the reader's face. <br />
+                        Jokes may also be added to the database, with the aim of making other users laugh.<br />
+                        Giggles main goal is to make people happy, make them smile and forget about any worries for a while.
+                    </h2>
+
                 <Button onClick={() => history.push('/login')}  text={'Register'} />
             </div>
         </main>
