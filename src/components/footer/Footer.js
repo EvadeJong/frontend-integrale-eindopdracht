006dc4d6c34cd0809ef87696f8278053e40d6f7b
EvadeJong <Eefjuh.de.jong@gmail.com>
2022-07-05 20:03:17 +0200

    footer changed

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Footer.css';
-import {NavLink} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 function Footer({text}) {
 
     return (
         <footer className='pageOuterContainer'>
             <div className='pageInnerContainer'>
-                <NavLink to='/contact' exact activeClassName='active-link'>{text}</NavLink>
+                <Link to='/contact' >{text}</Link>
             </div>
         </footer>
     );
