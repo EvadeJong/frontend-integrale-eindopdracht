d2b5d738626b1ab57d7b9f7301357f483e75ca94
EvadeJong <Eefjuh.de.jong@gmail.com>
2022-07-08 13:50:54 +0200

    tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import { act, cleanup, render } from '@testing-library/react'
-import { createMemoryHistory } from 'history';
-import { Router } from 'react-router';
+import {act, cleanup, render} from '@testing-library/react';
+import {createMemoryHistory} from 'history';
+import {Router} from 'react-router';
 
 import App from './App'
-import AuthContextProvider, { AuthContext } from './context/AuthContext';
+import AuthContextProvider, {AuthContext} from './context/AuthContext';
 
 afterEach(() => {
     cleanup();
@@ -11,11 +11,11 @@ afterEach(() => {
 
 it('4. Als niet-geregistreerde gebruiker heb je geen toegang tot de giggler pagina', async () => {
     // Arrange
-    const history = createMemoryHistory({forceRefresh:true});
+    const history = createMemoryHistory({forceRefresh: true});
     render(
         <Router history={history}>
             <AuthContextProvider>
-                <App />
+                <App/>
             </AuthContextProvider>
         </Router>
     )
@@ -29,11 +29,11 @@ it('4. Als niet-geregistreerde gebruiker heb je geen toegang tot de giggler pagi
 
 it('5. Als niet-geregistreerde gebruiker heb je geen toegang tot de joker pagina', async () => {
     // Arrange
-    const history = createMemoryHistory({forceRefresh:true});
+    const history = createMemoryHistory({forceRefresh: true});
     render(
         <Router history={history}>
             <AuthContextProvider>
-                <App />
+                <App/>
             </AuthContextProvider>
         </Router>
     )
@@ -47,11 +47,11 @@ it('5. Als niet-geregistreerde gebruiker heb je geen toegang tot de joker pagina
 
 it('6. Als niet-geregistreerde gebruiker heb je geen toegang tot de profiel pagina', async () => {
     // Arrange
-    const history = createMemoryHistory({forceRefresh:true});
+    const history = createMemoryHistory({forceRefresh: true});
     render(
         <Router history={history}>
             <AuthContextProvider>
-                <App />
+                <App/>
             </AuthContextProvider>
         </Router>
     )
@@ -65,11 +65,11 @@ it('6. Als niet-geregistreerde gebruiker heb je geen toegang tot de profiel pagi
 
 it('7. Als niet-geregistreerde gebruiker kan je je registreren', async () => {
     // Arrange
-    const history = createMemoryHistory({forceRefresh:true});
+    const history = createMemoryHistory({forceRefresh: true});
     render(
         <Router history={history}>
             <AuthContextProvider>
-                <App />
+                <App/>
             </AuthContextProvider>
         </Router>
     )
@@ -78,16 +78,16 @@ it('7. Als niet-geregistreerde gebruiker kan je je registreren', async () => {
         history.push('/Login')
     });
     // Assert
-    expect(history.location.pathname).toBe("/Login");
+    expect(history.location.pathname).toBe('/Login');
 });
 
 it('8. Als niet-geregistreerde gebruiker kan je contact opnemen', async () => {
     // Arrange
-    const history = createMemoryHistory({forceRefresh:true});
+    const history = createMemoryHistory({forceRefresh: true});
     render(
         <Router history={history}>
             <AuthContextProvider>
-                <App />
+                <App/>
             </AuthContextProvider>
         </Router>
     )
@@ -96,22 +96,22 @@ it('8. Als niet-geregistreerde gebruiker kan je contact opnemen', async () => {
         history.push('/Contact')
     });
     // Assert
-    expect(history.location.pathname).toBe("/Contact");
+    expect(history.location.pathname).toBe('/Contact');
 });
 
 it('9. Als geregistreerde gebruiker kan je inloggen', async () => {
     // Arrange
-    const history = createMemoryHistory({forceRefresh:true});
+    const history = createMemoryHistory({forceRefresh: true});
     const testContextData = {
         isAuth: true,
         user: {
-            username: "test"
+            username: 'test'
         }
     }
     render(
         <Router history={history}>
             <AuthContext.Provider value={testContextData}>
-                <App />
+                <App/>
             </AuthContext.Provider>
         </Router>
     )
@@ -120,22 +120,22 @@ it('9. Als geregistreerde gebruiker kan je inloggen', async () => {
         history.push('/Login')
     });
     // Assert
-    expect(history.location.pathname).toBe("/Login");
+    expect(history.location.pathname).toBe('/Login');
 });
 
 it('10. Als geregistreerde gebruiker kan je jouw profiel aanpassen', async () => {
     // Arrange
-    const history = createMemoryHistory({forceRefresh:true});
+    const history = createMemoryHistory({forceRefresh: true});
     const testContextData = {
         isAuth: true,
         user: {
-            username: "test"
+            username: 'test'
         }
     }
     render(
         <Router history={history}>
             <AuthContext.Provider value={testContextData}>
-                <App />
+                <App/>
             </AuthContext.Provider>
         </Router>
     )
@@ -144,5 +144,5 @@ it('10. Als geregistreerde gebruiker kan je jouw profiel aanpassen', async () =>
         history.push('/Profile')
     });
     // Assert
-    expect(history.location.pathname).toBe("/Profile");
+    expect(history.location.pathname).toBe('/Profile');
 });
